5e2aa8189813c3270b5004d31c49d54fa2d0324d
Santu Paul <paulsantu185@gmail.com>
2024-09-24 20:37:26 +0530

    .

--- backend/src/routes/adminRoutes/adminLogoutRoutes.js
+++ backend/src/routes/adminRoutes/adminLogoutRoutes.js
@@ -3,7 +3,7 @@ import express from 'express';
 const router = express.Router();
 router.post('/admin-logout', (req, res) => {
     res.clearCookie('adminToken', { path: '/' }); // Clear the cookie
-    res.sendStatus(200); // Respond with a success status
+    res.sendStatus(200);
 });
 
 export default router;
